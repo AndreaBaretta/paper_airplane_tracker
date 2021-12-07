689e9324f33b1d66a1b2e1e7244f76ca2c5a2e9a
Esther Kung <estherk3476@gmail.com>
2021-12-06 18:44:39 -0800

    fixed plot display // all graphs are rendered :D!

--- motion_graph.py
+++ motion_graph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('/home/andrea/Desktop/thingy.csv')
+df = pd.read_csv('/home/esther/Documents/airplane_trajectories/20-20_plain_2.csv')
 
 t = df.loc[:,'0']
 x = df.loc[:,'1']
@@ -10,3 +10,4 @@ y = df.loc[:,'2']
 
 plt.plot(x, y, '--ro', label='Airplane Path')
 plt.legend()
+plt.show()
